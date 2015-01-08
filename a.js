7b8f0f91ddf8803b1210e774b3f7278c4028b38e
naseroc <nicolas.melgarejos@gmail.com>
2015-01-08 13:05:19 -0300

    sc

---
+++ a.js
@@ -0,0 +1,28 @@
+/*var peso;
+var altura;
+var pesoideal;
+
+function imc(peso, altura) {
+	x = ()/()**2
+
+}
+
+function sobrepeso(imc) {
+}
+
+
+
+peso = prompt("Ingresa un númmero");
+peso = parseFloat(peso);
+
+altura = prompt("ingresa otro número");
+altura = parseFloat(altura);
+
+
+alert("El resultado de " + a + " + " + b + " es " + c); 
+*/
+
+function saluda(x,y) {
+	alert("Hola" + " " + x + " " + y);
+//	return "hola " + x + y;
+}
